2135c6b9ff2a7cf822cbb32c5cc783368a2b6b54
Golap31 <rosechowdhuryritu@gmail.com>
2025-04-21 01:34:47 +0600

    created database connection file dbcon.php

---
+++ crud_app/header.php
@@ -0,0 +1,12 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="
+sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1 id = "main_title"> Detailed Inventory Tracking of Products</h1>
+    <div class="container">
