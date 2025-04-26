07d5514415e3bf68775c727f4e472772f6bfb93c
Md. Riyad Hasan <hassanriyad666@gmail.com>
2025-04-26 20:19:50 +0600

    update css

--- Riyad/Preventive_measure.php
+++ Riyad/Preventive_measure.php
@@ -7,7 +7,6 @@ $dbname = "inventorymanagementsystem";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
@@ -17,12 +16,13 @@ $description = $conn->real_escape_string($_POST['description']);
 $implementation_date = $_POST['implementation_date'];
 $responsible_person = $conn->real_escape_string($_POST['responsible_person']);
 
-
 $sql = "INSERT INTO preventive_measures (measure_name, description, implementation_date, responsible_person)
         VALUES ('$measure_name', '$description', '$implementation_date', '$responsible_person')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New preventive measure added successfully. <a href='Preventive_measure.html'>Add Another</a>";
+ 
+  header("Location: view_measures.php");
+  exit();
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
